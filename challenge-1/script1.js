9732b29e3da900f4172ffe545329ed80bdebf21a
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-14 17:11:57 +0200

    created all files for challenge 1-3 and fixed code

---
+++ challenge-1/script1.js
@@ -0,0 +1,2 @@
+const value = "3";
+console.log(Number(value) + 4 + Number(value));
---
+++ challenge-2/script2.js
@@ -0,0 +1,10 @@
+const nickname = "Timmy";
+const firstname = "Timothy";
+
+if (nickname) {
+  console.log("Good Morning, " + nickname + "!");
+} else if (firstname) {
+  console.log("Good Morning, " + firstname + "!");
+} else {
+  console.log("Good Morning!");
+}
