bdbec4f9a1fbd33bb49b5042276998f9a140093b
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-16 15:46:50 +0200

    Update script3.js

--- challenge-3/script3.js
+++ challenge-3/script3.js
@@ -11,11 +11,10 @@ const divider = '----------------------------------';
 // Only change below this line
 
 const owed = (parseFloat(leoBalance) + parseFloat(sarahBalance)).toFixed(2); // Calculate owed amount
-const leo = `${leoName} ${leoSurname.trim()} (Owed: R ${Math.abs(parseFloat(leoBalance)).toFixed(2)})`;
-const sarah = `${sarahName.trim()} ${sarahSurname} (Owed: R ${Math.abs(parseFloat(sarahBalance)).toFixed(2)})`;
-const total = `Total amount owed: R ${Math.abs(owed).toFixed(2)}`;
+const leo = `${leoName} ${leoSurname.trim()} (Owed: R${Math.abs(parseFloat(leoBalance)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }).replace(/,/g, '.')})`;
+const sarah = `${sarahName.trim()} ${sarahSurname} (Owed: R${Math.abs(parseFloat(sarahBalance)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }).replace(/,/g, '.')})`;
+const total = `Total amount owed: R${Math.abs(owed).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }).replace(/,/g, '.')}`;
 const result = `${leo}\n${sarah}\n\n${divider}\n${total}\n${divider}`;
 
 console.log(result);
-
 
